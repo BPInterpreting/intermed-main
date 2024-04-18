08455439b10d67b03d67b828800d27c5acfa6436
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-04-17 21:21:38 -0700

    post route
    working on the post api route that includes a validator. asyncronously inserts new patient into the database

--- app/api/[[...route]]/patients.ts
+++ app/api/[[...route]]/patients.ts
@@ -1,12 +1,42 @@
 // authors.ts
 import { Hono } from 'hono'
+import { db } from '@/db/drizzle'
+import {patient} from "@/db/schema";
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
+//part of RPC is to create a schema for the validation that is used in the post request
+const schema = z.object({
+    firstName: z.string(),
+})
 
 const app = new Hono()
 
 
 // all the '/' routes are relative to the base path of this file which is /api/patients
-app.get('/', (c) => c.json('list patients'))
-app.post('/', (c) => c.json('create a patient', 201))
-app.get('/:id', (c) => c.json(`get ${c.req.param('id')}`))
+app.get('/', async (c) => {
+    // the get request will return all the patients in the database
+    const patients = await db.query.patient.findMany({
+        orderBy: (patient, {desc}) => [desc(patient.firstName)]
+    })
+    return c.json(patients)
+})
+
+app.post(
+    '/',
+    //TODO:add verify auth middleware
+    zValidator("form", schema),
+    async (c) => {
+        //access data from the frontend
+        const data = c.req.valid("form")
+
+        //TODO: throw HTTP exception from Hono if there is no user
+        const newPatient = await db.insert(patient).values({
+            firstName: data.firstName
+        })
+
+        return c.json(newPatient)
+})
+
 
 export default app
--- db/drizzle.ts
+++ db/drizzle.ts
@@ -3,7 +3,6 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql, { schema });
+export const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
 
-export default db
