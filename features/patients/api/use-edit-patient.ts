7037727de53f2e03c1205e4adb07f677f7a0cfcc
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-05-30 20:01:02 -0700

    use-edit-patient
    the api hook to edit patient is the mutation function that can be utilized in the front end.
    
    id is passed to make the query get the individual id of the patient that is being edited.

---
+++ features/patients/api/use-edit-patient.ts
@@ -0,0 +1,43 @@
+import {InferRequestType, InferResponseType} from "hono";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+
+import {client} from "@/lib/hono";
+import {toast} from "sonner";
+
+
+// these types help to know the type of object that will be requested and returned
+type ResponseType = InferResponseType<typeof client.api.patients[':id']['$patch']>
+//what the endpoint is expecting to be sent which needs the zValidator from the api accessed by json
+type RequestType = InferRequestType<typeof client.api.patients[':id']['$patch']>["json"]
+
+
+export const useCreatePatient = (id?:string ) => {
+    const queryClient = useQueryClient()
+
+    const mutation = useMutation<
+        ResponseType,
+        Error,
+        RequestType
+    > ({
+        //the mutation function is what is used to pass json and id
+        mutationFn: async (json) => {
+            const response = await client.api.patients[':id']['$patch']({
+                param: { id },
+                json
+            })
+            return await response.json()
+        },
+        onSuccess: () => {
+             //refetch all patients when a new patient is created to update the cache through the queryKey
+            toast.success('Patient updated successfully')
+            queryClient.invalidateQueries({ queryKey: ["patients"] })
+            queryClient.invalidateQueries({ queryKey: ["patient", { id }] });
+        },
+        onError: () => {
+            toast.error('Failed to update patient')
+            console.error('Failed to update patient')
+        }
+    })
+
+    return mutation
+}
