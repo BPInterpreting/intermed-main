03140968a5b29d7d0c8b128034ab931b253282c3
bpena707 <46693930+bpena707@users.noreply.github.com>
2024-09-04 15:32:17 -0700

    route protection
    implemented the clerk auth middleware to protect all routes in the app. now a user must be signed in in order to be granted access to any part of the app. this is for the client so far the routes still need to be protected

--- app/(dashboard)/appointments/actions.tsx
+++ app/(dashboard)/appointments/actions.tsx
@@ -37,8 +37,8 @@ export const Actions = ({id}: Props) => {
         return (
             <>
                 <ConfirmDialog/>
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
+                <DropdownMenu modal={false}>
+                    <DropdownMenuTrigger asChild >
                         <Button variant="ghost" className="h-8 w-8 p-0">
                             <MoreHorizontal className="h-4 w-4"/>
                         </Button>
