1badce362c170d6a2c83b632b62cc02975307f5c
bpena707 <46693930+bpena707@users.noreply.github.com>
2025-04-22 17:02:55 -0700

    route cors
    updated cors to also include custom domain

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -14,7 +14,7 @@ export const runtime = 'edge';
 //initialize the new hono api instance where base is out /api route
 const app = new Hono().basePath('/api')
 app.use('*', cors({
-    origin: 'http://localhost:8081',
+    origin: ['http://localhost:8081', 'https://www.pena-med.com'],
     allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
     allowMethods: ['POST', 'GET', 'OPTIONS'],
     exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
