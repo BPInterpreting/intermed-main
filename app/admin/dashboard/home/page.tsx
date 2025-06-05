5df357957ddf6bc61eafac335a68a04e841591b4
bpena707 <46693930+bpena707@users.noreply.github.com>
2025-06-04 18:57:59 -0700

    Homepage table
    included a simple table that renders the appointment data for the day so that the user can see all appointments listed on that day

--- app/admin/dashboard/home/page.tsx
+++ app/admin/dashboard/home/page.tsx
@@ -2,13 +2,19 @@
 
 import {DataGrid} from "@/components/customUi/data-grid";
 import {DataCharts} from "@/components/customUi/data-charts";
+import TodaysAppointmentTable from "@/components/customUi/todays-appointment-table";
 
 const HomePage =   () => {
 
     return (
-        <div className='flex flex-col mx-auto p-8'>
-            <DataGrid/>
-            <DataCharts/>
+        <div className='flex flex-col mx-2 p-4 gap-y-8'>
+            <div className='flex flex-col '>
+                <DataGrid/>
+                <DataCharts/>
+            </div>
+            <div>
+                <TodaysAppointmentTable/>
+            </div>
         </div>
     )
 }
