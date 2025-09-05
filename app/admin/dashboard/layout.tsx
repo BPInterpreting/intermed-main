74d5e6bdf84700293ad740a8f368ee58afcb301b
bpena707 <46693930+bpena707@users.noreply.github.com>
2025-09-05 09:04:58 -0700

    sidebar and nav items
    hooked up the sidebar with inset and gave the main page a boxy look. sidebar can expand or contract based on toggle.

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 import {Navbar} from "@/app/admin/dashboard/components/navbar";
 import {Sidebar} from "@/app/admin/dashboard/components/sidebar";
+import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
+import {AppSidebar} from "@/components/app-sidebar";
+import {SiteHeader} from "@/components/site-header";
 
 export default function DashboardLayout({
 children, // will be a page or nested layout
@@ -7,18 +10,14 @@ children, // will be a page or nested layout
     children: React.ReactNode
 }) {
     return (
-        <>
-            <div>
-                <Navbar/>
-            </div>
-            <div>
-                <Sidebar className='hidden md:flex '/>
-            </div>
-            <main className='md:pl-60 h-full pt-[80px]  mt-5 '>
-                <div className='h-full'>
-                    {children}
-                </div>
-            </main>
-        </>
+        <SidebarProvider>
+            <AppSidebar variant='inset' />
+                <SidebarInset>
+                    <SiteHeader />
+                    <main>
+                        {children}
+                    </main>
+                </SidebarInset>
+        </SidebarProvider>
     )
 }
