943c5af1d7d4d5288f0b3d390d472ab17e1147d0
bpena707 <46693930+bpena707@users.noreply.github.com>
2026-01-20 21:30:02 -0800

    Update client initialization in hono.ts to include credentials in fetch requests
    - Modified the client export to include a custom fetch function that adds credentials to requests, enhancing security and ensuring proper session handling.

--- lib/hono.ts
+++ lib/hono.ts
@@ -2,4 +2,7 @@ import { hc } from 'hono/client'; //creates the client
 import { AppType } from "@/app/api/[[...route]]/route"; //pass AppType as generics and URL is argument
 
 //hc is the function that creates the client. AppType is passed as generics and URL is passed as argument
-export const client = hc<AppType>(process.env.NEXT_PUBLIC_URL!)
+// export const client = hc<AppType>(process.env.NEXT_PUBLIC_URL!)
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_URL!, {
+  fetch: (input: RequestInfo | URL, init?: RequestInit) => fetch(input, { ...init, credentials: 'include' })
+})
